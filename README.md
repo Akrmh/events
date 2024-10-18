9dcf07e7c0edb1bf92170ff2b0f0f00a2a4b3af9
Akrmh <akrmh90@gmail.com>
2024-10-18 11:42:43 +0200

    signup page done

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+# Events
+An event is an action that couurs within a browser.
+
+An event can be something like :
+- a user clicking on a button.
+- typing in an input.
+- a page loading.
+
+## common Events
+- onclick
+- ondbcllick
+- onmouseover
+- onmouseout
+- onfoucs
---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+const handleClick = () =>{
+    const fullName = document.getElementById('fullname').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    console.log('Fullname: ',fullName);
+    console.log('Email: ',email);
+    console.log('password: ',password);
+    
+
+}
